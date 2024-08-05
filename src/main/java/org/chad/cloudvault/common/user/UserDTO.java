93debcfed7411b2deb9023e4ec23da4e44c093cc
Chad <2420811595@qq.com>
2024-08-05 19:52:20 +0800

    feature:搭建用户相关脚手架

---
+++ src/main/java/org/chad/cloudvault/common/user/UserDTO.java
@@ -0,0 +1,18 @@
+package org.chad.cloudvault.common.user;
+
+public class UserDTO {
+    /**
+     * 用户ID
+     */
+    private Long id;
+
+    /**
+     * 用户姓名
+     */
+    private String username;
+
+    /**
+     * 用户头像
+     */
+    private String icon = "";
+}
