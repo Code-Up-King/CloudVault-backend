7af38b9445de1b9ea0980ba43eb0c3badbc4ff3a
Chad <2420811595@qq.com>
2024-08-13 18:02:26 +0800

    feature：回收站相关操作

---
+++ src/main/java/org/chad/cloudvault/controller/RecoveryFileController.java
@@ -0,0 +1,37 @@
+package org.chad.cloudvault.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import lombok.RequiredArgsConstructor;
+import org.chad.cloudvault.domain.entity.Result;
+import org.chad.cloudvault.domain.vo.FileInfoPageVO;
+import org.chad.cloudvault.service.FileInfoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/recycle")
+public class RecoveryFileController {
+    private final FileInfoService fileInfoService;
+
+    @GetMapping("/list/{page}/{filePid}")
+    public Result<IPage<FileInfoPageVO>> recycleFileList(@PathVariable Integer page, @PathVariable Long filePid){
+        return fileInfoService.recycleFileList(page, filePid);
+    }
+
+    @DeleteMapping("/add")
+    public Result<Void> addRecycleFile(@RequestBody List<Long> ids){
+        return fileInfoService.addRecycleFile(ids);
+    }
+
+    @PutMapping("/recovery")
+    public Result<Void> recovery(@RequestBody List<Long> ids){
+        return fileInfoService.recovery(ids);
+    }
+
+    @DeleteMapping("/delete")
+    public Result<Void> deleteByFileIds(@RequestBody List<Long> ids){
+        return fileInfoService.deleteByFileIds(ids);
+    }
+}
